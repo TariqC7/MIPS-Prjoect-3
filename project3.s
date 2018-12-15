b6c99fcf8051494310686cefa72969d35083739f
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2018-12-14 20:53:51 -0500

    Just initializing the variables

---
+++ project3.s
@@ -0,0 +1,5 @@
+.data
+    noInput: .asciiz "Input is empty." #String with an empty input
+    tooLong: .asciiz "Input it is too long." #String that has more than 4 characters
+    invalidInput: .asciiz "Invalid base-35 number." #String that includes one or more characters not in set
+    userInput: .space 1000
